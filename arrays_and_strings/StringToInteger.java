2db493174347337d7c750caa1cb770eed83075b5
Di <divyabharadwajyellapragada@gmail.com>
2016-05-05 14:45:55 -0700

    Error handling

--- arrays_and_strings/StringToInteger.java
+++ arrays_and_strings/StringToInteger.java
@@ -5,13 +5,19 @@ public class StringToInteger {
 	public static int convertStringToInt(String s) {
 		boolean isNeg = false;
 		int i = 0, num = 0;
-
+		if(s == null) {
+			return;
+		}
 		if(s.charAt(i) == '-') {
 			isNeg = true;
 			i = 1;
 		}
 
 		while(i<s.length()) {
+			if(!(Character.isDigit(s.charAt(i))){
+				System.out.println("Enter a valid string that contains only numbers!");
+				return;
+			}
 			num = num * 10;
 			num = num + s.charAt(i) - '0';
 			i++;
