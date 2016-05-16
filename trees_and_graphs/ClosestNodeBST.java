b2c8b5f7b77f485d64ff7d3107760689547316c9
Di <divyabharadwajyellapragada@gmail.com>
2016-05-16 00:55:50 -0700

    Update ClosestNodeBST.java

--- trees_and_graphs/ClosestNodeBST.java
+++ trees_and_graphs/ClosestNodeBST.java
@@ -2,16 +2,18 @@
 
 public Node closestNode(Node root, int value){
 
-    if(root == null || root.value == value){
+    if(root.value == value){
       return root;
     }
-    else if(!root.left && root.value > value){
-      Node left = closestNode(root.left, value);
-      return Math.abs(left.value - value) > Math.abs(root.value - value) ? root : left;
+    else if(root.value > value){
+        if(!root.left) return root;
+        Node left = closestNode(root.left, value);
+        return Math.abs(left.value - value) > Math.abs(root.value - value) ? root : left;
     }
     else if(!root.right && root.value < value){
-      Node right = closestNode(root.right, value);
-      return Math.abs(right.value - value) > Math.abs(root.value - value) ? root : right;
+        if(!root.right) return root;
+        Node right = closestNode(root.right, value);
+        return Math.abs(right.value - value) > Math.abs(root.value - value) ? root : right;
     }
     return null;
   }
