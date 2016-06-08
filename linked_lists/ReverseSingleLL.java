6f3d0b475534960c3b339c9f25199ac6b4338e92
Di <divyabharadwajyellapragada@gmail.com>
2016-06-08 02:36:57 -0700

    Update ReverseSingleLL.java

--- linked_lists/ReverseSingleLL.java
+++ linked_lists/ReverseSingleLL.java
@@ -40,6 +40,27 @@ public class ReverseSingleLL {
 		head = rest;
 		return head;
 	}
+	
+	Node reverseUtil(Node curr, Node prev) {
+ 
+        /* If last node mark it head*/
+        if (curr.next == null) {
+            head = curr;
+ 
+            /* Update next to prev node */
+            curr.next = prev;
+            return null;
+        }
+ 
+        /* Save curr->next node for recursive call */
+        Node next1 = curr.next;
+ 
+        /* and update next ..*/
+        curr.next = prev;
+ 
+        reverseUtil(next1, curr);
+        return head;
+    }
 
 
 }
